ca92aaa4dcc69c1101e687bcfbe11743dd0ab4d9
Yes-said <bobsaid358@gmail.com>
2024-04-02 13:13:33 +0300

    update navbar,app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="https://bulma.io">
+        <svg width="640" height="160" viewBox="0 0 640 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+          {/* Your SVG content */}
+        </svg>
+      </a>
+
+      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-start">
+        <a className="navbar-item">
+          Home
+        </a>
+
+        <a className="navbar-item">
+          Documentation
+        </a>
+
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">
+            More
+          </a>
+
+          <div className="navbar-dropdown">
+            <a className="navbar-item">
+              About
+            </a>
+            <a className="navbar-item is-selected">
+              Jobs
+            </a>
+            <a className="navbar-item">
+              Contact
+            </a>
+            <hr className="navbar-divider"></hr>
+            <a className="navbar-item">
+              Report an issue
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a className="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a className="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
