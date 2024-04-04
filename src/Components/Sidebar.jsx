3805e3649b265995164fa806b8dba995a88edcb2
Yes-said <bobsaid358@gmail.com>
2024-04-04 20:28:37 +0300

    update sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className="columns">
-      <div className="column is-one-quarter mt-6"> 
-        <aside className="menu notification is-primary">
+    <div className="columns" style={{ height: '80vh', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+      {/* Adjusted height */}
+      <div className="column is-one-quarter mt-4">
+        <aside className="menu notification is-primary" style={{ height: '100%', width: '250px' }}> {/* Adjusted width */}
           <p className="menu-label">Menu</p>
           <ul className="menu-list">
             <li><a href="#" style={{ textDecoration: 'none', color: '#666' }}>Dashboard</a></li>
