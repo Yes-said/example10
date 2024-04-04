769c5235816f8a1ff8a4348d58af20d6f3ad4d05
Yes-said <bobsaid358@gmail.com>
2024-04-04 17:04:49 +0300

    footer updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer is-fixed-bottom has-background-primary has-text-white">
+    <footer className="footer is-fixed-bottom has-background-primary has-text-white" style={{ padding: '0.5rem 0' }}>
     <div className="content has-text-centered">
       <p>
         &copy; KIMC 2024 <strong>All rights reserved.</strong>
